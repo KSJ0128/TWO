aec650775506fdb4d203ba78359a3bec748a8606
KSJ0128 <ksj01128@naver.com>
2024-07-04 16:52:15 +0900

    Verify Controller, Service

--- TWO/src/main/java/com/togetherwithocean/TWO/User/Service/UserService.java
+++ TWO/src/main/java/com/togetherwithocean/TWO/User/Service/UserService.java
@@ -2,7 +2,6 @@ package com.togetherwithocean.TWO.User.Service;
 
 import com.togetherwithocean.TWO.User.Domain.User;
 import com.togetherwithocean.TWO.User.Repository.UserRepository;
-import com.togetherwithocean.TWO.Verify.SmsUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,18 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final SmsUtil smsUtil;
 
     @Transactional
     public User getUserByRealName(String realName) {
         return userRepository.findByRealName(realName);
     }
 
-    @Transactional
-    public String createVerifyNumber() {
-        int randomNumber = (int)(Math.random() * 10000) + (int)(Math.random() * 100);
-        String verifyNumber = String.valueOf(randomNumber);
-
-        return verifyNumber;
-    }
 }
--- TWO/src/main/java/com/togetherwithocean/TWO/Verify/SmsDao.java
+++ TWO/src/main/java/com/togetherwithocean/TWO/Verify/SmsDao.java
@@ -9,27 +9,25 @@ import java.time.Duration;
 @RequiredArgsConstructor
 @Repository
 public class SmsDao {
-    private final String PREFIX = "sms:";
+    private final String PREFIX = "SMS:";
     private final int LIMIT_TIME = 3 * 60;
 
     private final StringRedisTemplate redisTemplate;
 
     public void createSmsCertification(String phone, String certificationNumber) {
-        System.out.println("여기까지는 오니?");
         redisTemplate.opsForValue()
                 .set(PREFIX + phone, certificationNumber, Duration.ofSeconds(LIMIT_TIME));
-        System.out.println("여기까지 오니?");
     }
 
     public String getSmsCertification(String phone) {
         return redisTemplate.opsForValue().get(PREFIX + phone);
     }
 
-    public void removeSmsCertification(String phone) {
-        redisTemplate.delete(PREFIX + phone);
-    }
-
-    public boolean hasKey(String phone) {
-        return redisTemplate.hasKey(PREFIX + phone);
-    }
+//    public void removeSmsCertification(String phone) {
+//        redisTemplate.delete(PREFIX + phone);
+//    }
+//
+//    public boolean hasKey(String phone) {
+//        return redisTemplate.hasKey(PREFIX + phone);
+//    }
 }
