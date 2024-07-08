8280238858261d72f26aed0b7a45d15c2b240de4
KSJ0128 <ksj01128@naver.com>
2024-07-08 20:44:26 +0900

    SCURM-66 매달 1일 멤버별 월 줍깅 수 및 스코어 0으로 초기화

--- TWO/src/main/java/com/togetherwithocean/TWO/Member/Repository/MemberRepository.java
+++ TWO/src/main/java/com/togetherwithocean/TWO/Member/Repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.togetherwithocean.TWO.Member.Repository;
 import com.togetherwithocean.TWO.Member.Domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -10,4 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
     boolean existsByEmail(String email);
     Member findMemberByEmail(String email);
+
+    List<Member> findAll();
 }
--- TWO/src/main/java/com/togetherwithocean/TWO/Plogging/Service/PloggingService.java
+++ TWO/src/main/java/com/togetherwithocean/TWO/Plogging/Service/PloggingService.java
@@ -1,4 +1,26 @@
 package com.togetherwithocean.TWO.Plogging.Service;
 
+import com.togetherwithocean.TWO.Member.Domain.Member;
+import com.togetherwithocean.TWO.Member.Repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class PloggingService {
+    private final MemberRepository memberRepository;
+
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void initPloggingTrashNScore() {
+        List<Member> allMember = memberRepository.findAll();
+
+        for (int i = 0; i < allMember.size(); i++) {
+            allMember.get(i).setMonthlyTrashBag(0L);
+            allMember.get(i).setMonthlyScore(0L);
+            memberRepository.save(allMember.get(i));
+        }
+    }
 }
