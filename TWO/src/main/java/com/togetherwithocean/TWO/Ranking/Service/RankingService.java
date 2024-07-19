d8f05b4e1b21f0e25924558ddcabcb60726b29fa
KSJ0128 <ksj01128@naver.com>
2024-07-20 01:01:53 +0900

    SCRUM-75 보리고래 배지 획득-랭킹 1등

--- TWO/src/main/java/com/togetherwithocean/TWO/Ranking/Service/RankingService.java
+++ TWO/src/main/java/com/togetherwithocean/TWO/Ranking/Service/RankingService.java
@@ -1,20 +1,32 @@
 package com.togetherwithocean.TWO.Ranking.Service;
 
+import com.togetherwithocean.TWO.Badge.Domain.Badge;
+import com.togetherwithocean.TWO.Badge.Repository.BadgeRepository;
 import com.togetherwithocean.TWO.Member.Domain.Member;
 import com.togetherwithocean.TWO.Member.Repository.MemberRepository;
+import com.togetherwithocean.TWO.MemberBadge.Domain.MemberBadge;
+import com.togetherwithocean.TWO.MemberBadge.Repository.MemberBadgeRepository;
 import com.togetherwithocean.TWO.Ranking.DTO.RankingDTO;
 import com.togetherwithocean.TWO.Ranking.Domain.Ranking;
 import com.togetherwithocean.TWO.Ranking.Repository.RankingRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RankingService {
+    @Autowired
     private final RankingRepository rankingRepository;
+    @Autowired
+    private final MemberBadgeRepository memberBadgeRepository;
+    @Autowired
+    private final BadgeRepository badgeRepository;
     private final MemberRepository memberRepository;
 
     public Ranking makeNewRanking(Member member) {
@@ -49,5 +61,29 @@ public class RankingService {
             }
         }
         return myRankingDTO;
+    }
+
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void initMonthlyRanking() {
+        List<Ranking> rankingList = rankingRepository.findAllByOrderByScoreDesc();
+
+        for (Ranking ranking : rankingList) {
+            if (ranking.equals(rankingList.getFirst())) {
+                Member member = ranking.getMember();
+
+                Badge badge = badgeRepository.findBadgeByBadgeNumber(10L);
+
+                if (memberBadgeRepository.findMemberBadgeByMemberAndBadge(member, badge) == null) {
+                    // 보리고래 배지 지급
+                    MemberBadge memberBadge = MemberBadge.builder()
+                            .member(member)
+                            .badge(badge)
+                            .build();
+                    memberBadgeRepository.save(memberBadge);
+                }
+            }
+            ranking.setScore(0L);
+            rankingRepository.save(ranking);
+        }
     }
 }
