161863532b7ea75dba37bb31d52efc03938e102c
성유진 <syj010820@naver.com>
2024-07-21 20:18:30 +0900

    환경변수 설정

--- TWO/src/main/java/com/togetherwithocean/TWO/OAuth/Controller/AuthController.java
+++ TWO/src/main/java/com/togetherwithocean/TWO/OAuth/Controller/AuthController.java
@@ -9,21 +9,27 @@ import com.togetherwithocean.TWO.OAuth.DTO.KakaoUserInfo;
 import com.togetherwithocean.TWO.OAuth.Service.KakaoService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/oauth")
 @RequiredArgsConstructor
+@PropertySource("classpath:/application.properties")
 public class AuthController {
-    private String redirectUri = "http://localhost:8080/oauth/kakao/callback";
-    private String clientId = "4f70d41bf691ab5b26944b067390995b";
+    @Value("${redirectURI}")
+    private String redirectUri;
+    @Value("${clientID}")
+    private String clientId;
     private final KakaoService kakaoService;
     private final MemberService memberService;
     private final MemberRepository memberRepository;
 
     @GetMapping("/kakao/login")
     public ResponseEntity<String> kakaoLogin() {
+        System.out.println("kakao login");
         String kakaoLoginUrl =
                 "https://kauth.kakao.com/oauth/authorize?client_id=" + clientId
                         + "&redirect_uri=" + redirectUri + "&response_type=code";
@@ -32,11 +38,12 @@ public class AuthController {
 
     @GetMapping("/kakao/callback")
     public @ResponseBody ResponseEntity<Member> kakaoCallback(@RequestParam String code, HttpServletResponse response) {
+        System.out.println("kakao callback");
         String accessToken = kakaoService.getAccessToken(code);
         KakaoUserInfo kakaoUserInfo = kakaoService.getUserInfoByAccessToken(accessToken);
-//        System.out.println(kakaoUserInfo.getName());
-//        System.out.println(kakaoUserInfo.getEmail());
-//        System.out.println(kakaoUserInfo.getPhoneNumber());
+        System.out.println(kakaoUserInfo.getName());
+        System.out.println(kakaoUserInfo.getEmail());
+        System.out.println(kakaoUserInfo.getPhoneNumber());
 
         // 카카오에서 가져온 정보 바탕으로 사용자 찾기
         Member loginMember = memberRepository.findMemberByEmail(kakaoUserInfo.getEmail());
